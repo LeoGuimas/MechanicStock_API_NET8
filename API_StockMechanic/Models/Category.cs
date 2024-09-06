4dcf545292a72127cb7481cbc17c22ba44369e1d
LeoGuimas <gleo.dev0@gmail.com>
2024-09-05 22:28:19 -0300

    some alterations in products and categories

--- API_StockMechanic/Models/Category.cs
+++ API_StockMechanic/Models/Category.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_StockMechanic.Models
 {
+    [Table("Categories")]
     public class Category
     {
         public Category() { 
         
             Products = new Collection<Product>();
         }
+
+        [Key]
         public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? Name { get; set; }
+        
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
 
--- API_StockMechanic/Models/Product.cs
+++ API_StockMechanic/Models/Product.cs
@@ -1,13 +1,30 @@
-﻿namespace API_StockMechanic.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_StockMechanic.Models
 {
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
+        [StringLength(80)]
+        [Required]
         public string? Name { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? Description { get; set; }
+
+        [Required]
+        [Column(TypeName ="decimal(10,2)")]
         public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImageUrl {  get; set; }
+
         public float Stock {  get; set; }
+
         public DateTime RegisterDate { get; set; }
 
 
